beb67fcf4f683f3f902a03d54c78746151a5a7c5
Prateek Soni <33653480+vallabhdev@users.noreply.github.com>
2022-02-10 13:51:04 -0500

    updating logic to use BeautifulSoup library.

--- XmlToCsvConverter.py
+++ XmlToCsvConverter.py
@@ -1,24 +1,36 @@
-import xml.etree.ElementTree as xet
-
+# Python program to convert xml
+# structure into dataframes using beautifulsoup
 import pandas as pd
+# Import libraries
+from bs4 import BeautifulSoup
 
-parsedXml = xet.parse('myFile.xml')
-columns = ['name', 'phone', 'email', 'date', 'country']
-rows = []
+# Open XML file
+file = open("myFile.xml", 'r')
 
-root = parsedXml.getroot()
+# Read the contents of that file
+contents = file.read()
 
-for each in root:
-    name = each.find("name").text
-    phone = each.find("phone").text
-    email = each.find("email").text
-    date = each.find("date").text
-    country = each.find("country").text
+soup = BeautifulSoup(contents)
 
-    rows.append({"name": name,
-                 "phone": phone,
-                 "email": email,
-                 "date": date,
-                 "country": country})
-df = pd.DataFrame(rows, columns)
+# Extracting the data
+authors = soup.find_all('author')
+titles = soup.find_all('title')
+prices = soup.find_all('price')
+pubdate = soup.find_all('publish_date')
+genres = soup.find_all('genre')
+des = soup.find_all('description')
+
+data = []
+
+# Loop to store the data in a list named 'data'
+for i in range(0, len(authors)):
+    rows = [authors[i].get_text(), titles[i].get_text(), genres[i].get_text(),
+            prices[i].get_text(), pubdate[i].get_text(), des[i].get_text()]
+    data.append(rows)
+
+# Converting the list into dataframe
+df = pd.DataFrame(data, columns=['Author',
+                                 'Book Title', 'Genre',
+                                 'Price', 'Publish Date',
+                                 'Description'], dtype=str)
 df.to_csv('myFile.csv')
