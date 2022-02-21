7299365fe47c5ab5408b9d529ed3cef8d59df5c0
AkhilAkoliya <98978265+AkhilAkoliya@users.noreply.github.com>
2022-02-21 18:59:16 -0500

    Update api data.py

--- api data.py
+++ api data.py
@@ -2,13 +2,9 @@ import json
 import csv
 import requests
 
-"""response = requests.get("https://animals-endangered-environmentalism.p.rapidapi.com/population/1")
-print(response.status_code)"""
+url = "https://animals-endangered-environmentalism.p.rapidapi.com/taxonomy/class"
 
-
-url = "https://animals-endangered-environmentalism.p.rapidapi.com/population/1"
-
-querystring = {"type":"equal"}
+querystring = {"q":"mammalia"}
 
 headers = {
     'x-rapidapi-host': "animals-endangered-environmentalism.p.rapidapi.com",
@@ -16,60 +12,106 @@ headers = {
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
+
+
 apidata=response.text
 apidata.encode("utf-8")
 fetchdata=json.loads(apidata)
-#print(type(p))
+#print(fetchdata[0])
+
+
+scientificname=[]
 family=[]
+Class=[]
+yearassessed=[]
 commonname=[]
+yearpublished=[]
+kingdom=[]
+phylum=[]
+populationTrend=[]
+genus=[]
+numberofmatureindividuals=[]
+
+
+native=[]
+numberoflocation=[]
+extantResident=[]
+continuingDeclineOfMatureIndividuals=[]
+currentPopulationTrend=[]
+threats=[]
+habitatAndEcologysystem=[]
+generationLengthYears=[]
+habitatType=[]
+habitatname=[]
+conservationActions=[]
 iucnRedListCategoryAndCriteria=[]
-estimatedExtentOfOccurrenceEooKm2=[]
+useAndTrade=[]
+
+
+
+
+
+
+"""estimatedExtentOfOccurrenceEooKm2=[]
 estimatedAreaOfOccupancyAooKm2=[]
 extremeFluctuationsInExtentOfOccurrenceEoo=[]
 extremeFluctuationsInTheNumberOfLocations=[]
-extantResident=[]
-habitatType=[]
-generationLengthYears=[]
 #residentialCommercialDevelopment=[]
-#humanIntrusionsDisturbance=[]
-threats=[]
-useAndTrade=[]
-populationTrend=[]
-Class=[]
+#humanIntrusionsDisturbance=[]"""
 
-
-"""i = 0
-for i in range(len(fetchdata)):"""
 
 
 i=0
 for i in range(len(fetchdata)):
+    scientificname.append(fetchdata[i]['scientificName'])
     family.append(fetchdata[i]['family'])
+    Class.append(fetchdata[i]['class'])
+    yearassessed.append((fetchdata[i]['yearAssessed']))
     commonname.append(fetchdata[i]['commonName'])
-    iucnRedListCategoryAndCriteria.append(fetchdata[i]['data']['assessmentInformation']['iucnRedListCategoryAndCriteria'])
-    estimatedExtentOfOccurrenceEooKm2.append(fetchdata[i]['data']['geographicRange']['details']['estimatedExtentOfOccurrenceEooKm2'])
-    estimatedAreaOfOccupancyAooKm2.append(fetchdata[i]['data']['geographicRange']['details']['estimatedAreaOfOccupancyAooKm2'])
-    extremeFluctuationsInExtentOfOccurrenceEoo.append(fetchdata[i]['data']['geographicRange']['details']['extremeFluctuationsInExtentOfOccurrenceEoo'])
-    extremeFluctuationsInTheNumberOfLocations.append(fetchdata[i]['data']['geographicRange']['details']['extremeFluctuationsInTheNumberOfLocations'])
+    yearpublished.append((fetchdata[i]['yearPublished']))
+    kingdom.append(fetchdata[i]['kingdom'])
+    phylum.append(fetchdata[i]['phylum'])
+    populationTrend.append(fetchdata[i]['populationTrend'])
+    genus.append(fetchdata[i]['genus'])
+    numberofmatureindividuals.append((fetchdata[i]['numberOfMatureIndividuals']))
+
+
+
+
+    try:
+        native.append(fetchdata[i]['data']['geographicRange']['native'])
+    except:
+        pass
+    numberoflocation.append(fetchdata[i]['data']['geographicRange']['numberOfLocations'])
     try:
         extantResident.append(fetchdata[i]['data']['geographicRange']['extantResident'])
     except:
-        extantResident.append('NULL')
-    habitatType.append(fetchdata[i]['data']['habitatAndEcology']['habitatType'])
-    generationLengthYears.append(fetchdata[i]['data']['habitatAndEcology']['generationLengthYears'])
-    #residentialCommercialDevelopment.append(fetchdata[i]['data']['threats']['residential&CommercialDevelopment'][0])
+        pass
+        #extantResident.append('NULL')
+    continuingDeclineOfMatureIndividuals.append(fetchdata[i]['data']['population']['continuingDeclineOfMatureIndividuals'])
+    currentPopulationTrend.append(fetchdata[i]['data']['population']['currentPopulationTrend'])
     threats.append(fetchdata[i]['data']['threats'])
+    habitatAndEcologysystem.append(fetchdata[i]['data']['habitatAndEcology']['system'])
+    generationLengthYears.append(fetchdata[i]['data']['habitatAndEcology']['generationLengthYears'])
+    habitatType.append(fetchdata[i]['data']['habitatAndEcology']['habitatType'])
+    habitatname.append(fetchdata[i]['data']['habitatAndEcology']['name'])
+    conservationActions.append(fetchdata[i]['data']['conservationActions'])
+    iucnRedListCategoryAndCriteria.append(fetchdata[i]['data']['assessmentInformation']['iucnRedListCategoryAndCriteria'])
     useAndTrade.append(fetchdata[i]['data']['useAndTrade']['details']['useAndTrade'])
-    populationTrend.append(fetchdata[i]['populationTrend'])
-    Class.append(fetchdata[i]['class'])
 
-rows=zip(commonname,family,iucnRedListCategoryAndCriteria,estimatedExtentOfOccurrenceEooKm2,estimatedAreaOfOccupancyAooKm2,
-         extremeFluctuationsInExtentOfOccurrenceEoo,extremeFluctuationsInTheNumberOfLocations,extantResident,habitatType,
-         generationLengthYears,threats,useAndTrade,populationTrend,Class)
-header=['commonname','family','iucnRedListCategoryAndCriteria','estimatedExtentOfOccurrenceEooKm2','estimatedAreaOfOccupancyAooKm2',
-         'extremeFluctuationsInExtentOfOccurrenceEoo','extremeFluctuationsInTheNumberOfLocations','extantResident','habitatType',
-         'generationLengthYears','threats','useAndTrade','populationTrend','Class']
-with open('data.csv', "w") as f:
+
+
+
+rows=zip(scientificname,family,Class,yearassessed,commonname,yearpublished,kingdom,phylum,populationTrend,genus,
+         numberofmatureindividuals,native,numberoflocation,extantResident,continuingDeclineOfMatureIndividuals,
+         currentPopulationTrend,threats,habitatAndEcologysystem,generationLengthYears,habitatType,habitatname,
+         conservationActions,iucnRedListCategoryAndCriteria,useAndTrade)
+header=['scientificname','family','Class','yearassessed','commonname','yearpublished','kingdom','phylum','populationtrend','genus',
+         'numberofmatureindividuals','native','numberoflocation','extantresident','continuingDeclineOfMatureIndividuals',
+         'currentPopulationTrend','threats','habitatAndEcologysystem','generationLengthYears','habitatType','habitatname',
+         'conservationActions','iucnRedListCategoryAndCriteria','useAndTrade']
+
+with open('bytaxonomymammalia.csv', "w", newline="") as f:
     writer = csv.writer(f)
     writer.writerow(header)
     for row in rows:
@@ -77,14 +119,4 @@ with open('data.csv', "w") as f:
             writer.writerow(row)
         except:
             pass
-
-
-
-
-
-#print(len(extantResident))
-#print(extantResident)
-#print(fetchdata[i]['data']['assessmentInformation']['yearPublished'])
-#print(response.text)
-
 
