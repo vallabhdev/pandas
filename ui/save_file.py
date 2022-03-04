e003433221daef2fd041ad1306fdcb0c001ca0f3
prateek soni <vallabh_dev@yahoo.com>
2022-03-04 18:19:01 -0500

    harmonizing the gallery images

--- ui/save_file.py
+++ ui/save_file.py
@@ -1,17 +1,17 @@
 
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
+# from werkzeug import secure_filename
 app = Flask(__name__)
 
-@app.route('/upload')
-def upload_file():
-    return render_template('index.html')
+# @app.route('/upload')
+# def upload_file():
+#     return render_template('index.html')
 
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(secure_filename(f.filename))
+        # f.save(secure_filename(f.filename))
         return 'file uploaded successfully'
 
 if __name__ == '__main__':
