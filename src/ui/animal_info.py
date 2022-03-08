e26efcb84d4cb438060c0507b4cac040b8d600eb
prateek soni <vallabh_dev@yahoo.com>
2022-03-07 21:58:31 -0500

    modularizing the apis

--- src/ui/animal_info.py
+++ src/ui/animal_info.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from flask import render_template, Blueprint
+
+info_blueprint = Blueprint('info', __name__)
 
 
 def basic_details():
@@ -15,3 +18,35 @@ def endangered_status():
 
 def remediation():
     return pd.read_csv('../../data/Remediation_measures.csv').drop(columns=['Country', 'Effect_of_Measures'])
+
+
+@info_blueprint.route("/info/")
+def info():
+    title = "Details of Mammals under threat..."
+    df = basic_details()
+    return render_template("base.html", title=title, tables=[df.to_html(classes='data', header="true")],
+                           titles=df.columns.values)
+
+
+@info_blueprint.route("/factors")
+def factors():
+    title = "Factors affecting of existence of Mammals..."
+    df = affecting_factors()
+    return render_template("base.html", title=title, tables=[df.to_html(classes='data', header="true")],
+                           titles=df.columns.values, )
+
+
+@info_blueprint.route("/status")
+def status():
+    title = "Endangered Status of Mammals..."
+    df = endangered_status()
+    return render_template("base.html", title=title, tables=[df.to_html(classes='data', header="true")],
+                           titles=df.columns.values)
+
+
+@info_blueprint.route("/remediation")
+def remediation_steps():
+    title = "Remediation steps taken to protect these mammals..."
+    df = remediation()
+    return render_template("base.html", title=title, tables=[df.to_html(classes='data', header='true')],
+                           titles=df.columns.values)
--- src/ui/app.py
+++ src/ui/app.py
@@ -1,49 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask
 
 from animal_info import *
+from animal_info import info_blueprint
 from save_file import save_blueprint
 from user_info import *
 
 app = Flask(__name__)
 app.register_blueprint(save_blueprint)
+app.register_blueprint(info_blueprint)
 
 
 @app.route("/")
 def home():
     users = user_details()
     return render_template("index.html", users=users)
-
-
-@app.route("/info")
-def info():
-    title = "Details of Mammals under threat..."
-    df = basic_details()
-    return render_template("base.html", title=title, tables=[df.to_html(classes='data', header="true")],
-                           titles=df.columns.values)
-
-
-@app.route("/factors")
-def factors():
-    title = "Factors affecting of existence of Mammals..."
-    df = affecting_factors()
-    return render_template("base.html", title=title, tables=[df.to_html(classes='data', header="true")],
-                           titles=df.columns.values, )
-
-
-@app.route("/status")
-def status():
-    title = "Endangered Status of Mammals..."
-    df = endangered_status()
-    return render_template("base.html", title=title, tables=[df.to_html(classes='data', header="true")],
-                           titles=df.columns.values)
-
-
-@app.route("/remediation")
-def remediation_steps():
-    title = "Remediation steps taken to protect these mammals..."
-    df = remediation()
-    return render_template("base.html", title=title, tables=[df.to_html(classes='data', header='true')],
-                           titles=df.columns.values)
 
 
 if __name__ == '__main__':
