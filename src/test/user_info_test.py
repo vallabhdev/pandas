8f0e64ecd6c39d9fced19e133625833392c7f7ff
AkhilAkoliya <akhilakoliya@gmail.com>
2022-03-10 17:00:53 -0500

    Improving assertions

--- src/test/user_info_test.py
+++ src/test/user_info_test.py
@@ -5,15 +5,17 @@ from src.ui.user_info import *
 
 class UserInfoTest(unittest.TestCase):
     def testing(self):
-        dictionary = {'Jinal': ['Leader'], 'Akhil': ['Python Developer'], 'Ankit': ['WebScrapper'],
+        expected_dict = {'Jinal': ['Leader'], 'Akhil': ['Python Developer'], 'Ankit': ['WebScrapper'],
                       'Chirag': ['Data Engineer'],
                       'Chaitali': ['ML Developer'], 'Ravi': ['Data Modeler'], 'Suchith': ['Data Modeler'],
                       'Celeste': ['Data Engineer'], 'Misha': ['Data Analyst'], 'Krutika': ['Data Engineer'],
                       'Prakshi': ['Data Engineer'], 'Prateek': ['UI Developer']}
-        self.assertAlmostEqual(len(user_details()), 12)
-        self.assertIsNotNone(user_details().keys())
-        self.assertIsNotNone(user_details().values())
-        self.assertDictEqual(user_details(), dictionary)
+        actual_dictionary = user_details()
+        self.assertAlmostEqual(len(actual_dictionary), 12)
+        self.assertAlmostEqual(len(actual_dictionary.keys()), 12)
+        self.assertAlmostEqual(len(actual_dictionary.values()), 12)
+        self.assertAlmostEqual(actual_dictionary.get('Jinal'),['Leader'])
+        self.assertDictEqual(actual_dictionary, expected_dict)
 
 
 if __name__ == "__main__":
