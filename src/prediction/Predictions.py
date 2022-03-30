ca790b7b12d15a9b75721a92b1e9869e8f765fa5
Chirag Beladiya <cabeladiya@gmail.com>
2022-03-30 19:30:16 -0400

    Adding text field for animal name

--- src/prediction/Predictions.py
+++ src/prediction/Predictions.py
@@ -1,5 +1,11 @@
 import pandas as pd
 from flask import Blueprint
+from flask import Flask, request, render_template
+from src.ui import animal_info
+from src.ui.user_info import user_details
+import json
+import plotly
+import plotly.express as px
 
 predict_blueprint = Blueprint('predict', __name__)
 
@@ -20,10 +26,19 @@ def filter_df_for(animal_name, df):
 
 def load_data():
     return pd.read_csv('../../data/Population_by_year.csv')
-
-
-@predict_blueprint.route('/predict/<name>/<year>', methods=['GET'])
-def predict_population(name, year):
+#
+# def get_plot_data(name):
+#     df = load_data()
+#     df = filter_df_for(name, df)
+#     X = df['Year']
+#     Y = df['Population']
+#     return {'x_axis': X, 'y_axis': Y}
+#, methods=["GET",'POST']
+@predict_blueprint.route('/predict',methods=["GET","POST"])
+def predict_population():
+    global pred_y
+    name = request.args.get("name")
+    year = request.args.get("year")
     df = load_data()
     df = filter_df_for(name, df)
     X = df['Year']
@@ -31,4 +46,27 @@ def predict_population(name, year):
     m, b = linear_regression_for(X, Y)
     yr = int(year)
     y_hat = m * yr + b
-    return str(y_hat)
+    pred_y = None
+    if y_hat <= 0:
+        pred_y = 0
+    else:
+        pred_y = y_hat
+    print(X,Y)
+    # fig = px.line(X,Y)
+    # fig.show()
+    # graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return str(pred_y)
+
+
+
+# @predict_blueprint.route('/predict/<name>/<year>', methods=['GET'])
+# def predict_population():
+#     my_form_post(name,year)
+#     df = load_data()
+#     df = filter_df_for(name, df)
+#     X = df['Year']
+#     Y = df['Population']
+#     m, b = linear_regression_for(X, Y)
+#     yr = int(year)
+#     y_hat = m * yr + b
+#     return str(y_hat)
